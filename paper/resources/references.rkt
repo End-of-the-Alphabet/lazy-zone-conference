407c3791131ffe45bb857b19a703ab79007c0a7b
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2021-04-13 14:37:45 -0600

    Add asteroids wiki page reference

--- paper/resources/references.rkt
+++ paper/resources/references.rkt
@@ -2,10 +2,12 @@
 
 (provide (all-defined-out))
 
+(require scribble/base)
 (require scriblib/autobib)
 
 (define-cite ~cite citet generate-bibliography
-  #:style author+date-square-bracket-style)
+  #:style number-style)
+  ;; #:style author+date-square-bracket-style)
 
 (define knuth-art
   (make-bib #:title "The Art of Computer Programming"
@@ -26,7 +28,7 @@
             #:url "https://en.wikipedia.org/w/index.php?title=Tabu_search&oldid=1006854633"))
 
 (define wiki-asteroids
-  (make-bib #:title @{Wikipedia: @italic{Asteroids}}
+  (make-bib #:title (elem "Wikipedia: " (italic "Asteroids") " (video game)")
             #:author (authors "Wikipedia")
             #:date 2021
             #:url "https://en.wikipedia.org/w/index.php?title=Asteroids_(video_game)&oldid=1016830718"))
